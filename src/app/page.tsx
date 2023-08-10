807dd067f214169f2def2eaf2acbbc35a189bceb
caio-mendonca <caioeduardojm4@gmail.com>
2023-08-10 19:46:24 -0400

    rewrite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const teste = process.env.TESTE
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          variaveis de ambiente { process.env.TESTE}
+          variaveis de ambiente: {teste}
         </p>
       </div>
-
-
-
     </main>
   )
 }
